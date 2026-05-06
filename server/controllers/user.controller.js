40c2216a432febcf3bcc8a879b8304b1c1732807
pratik kad <148075940+pratikkad10@users.noreply.github.com>
2026-05-06 12:11:19 +0530

    fix: added error logs to error causing controllers

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -98,20 +98,13 @@ export const logoutController = async (req, res) => {
 
 export const getmeController = async (req, res) => {
     try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({ error: "Unauthorized" });
-        }
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        if (!decoded) {
-            return res.status(401).json({ error: "Invalid token" });
-        }
-        const user = await User.findById(decoded.userId).select("-password");
+        const user = await User.findById(req.user._id).select("-password");
         if (!user) {
             return res.status(404).json({ error: "User not found" });
         }
         res.status(200).json(user);
     } catch (error) {
+        console.log(error);
         res.status(500).json({ error: "Internal server error" });
     }
 }
